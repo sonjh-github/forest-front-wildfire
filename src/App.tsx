e2d96b7fece2b5b23c869ae678f95b536aadcba5
tobeunicorn1012 <tobeunicorn1012@gmail.com>
2026-08-27 09:21:48 +0900

    feat: add dedicated device management page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import UnifiedDisasterDashboard from "./features/disaster-dashboard/UnifiedDisasterDashboard";
+import DeviceManagementPage from "./pages/device/DeviceManagementPage";
 import "./styles.css";
 
+function normalizedPath() {
+  const path = window.location.pathname.replace(/\/+$/, "");
+  return path || "/";
+}
+
 export default function App() {
+  const path = normalizedPath();
+
+  if (path === "/device") {
+    return <DeviceManagementPage />;
+  }
+
   return (
     <div className="app-shell">
       <UnifiedDisasterDashboard />
